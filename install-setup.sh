8a13e3d69e9ca39a480df168dcf9407cfae4f22f
anasir514 <anasir514@gmail.com>
2021-01-28 09:12:08 +0000

    Update install-setup.sh

--- install-setup.sh
+++ install-setup.sh
@@ -8,15 +8,10 @@ dpkg -l | grep -qw gdebi || sudo apt-get install -yyq gdebi
 
 sudo apt update
 
-sudo apt install virtualbox virtualbox-guest-additions-iso virtualbox-ext-pack \
-net-tools htop lame git mc flatpak audacity \
-openssh-server sshfs gedit-plugin-text-size simplescreenrecorder nano \
-ubuntu-restricted-extras mpv vlc gthumb gnome-tweaks \
-gnome-tweak-tool qt5-style-plugins spell synaptic -yy
-
-# Add me to any groups I might need to be a part of:
-
-sudo adduser $USER vboxusers
+sudo apt install net-tools htop lame git mc flatpak \
+gedit-plugin-text-size  nano \
+ubuntu-restricted-extras mpv vlc gnome-tweaks \
+gnome-tweak-tool qt5-style-plugins  synaptic -yy
 
 # Remove undesirable packages:
 
@@ -28,18 +23,6 @@ sudo snap remove gnome-characters gnome-calculator gnome-system-monitor
 sudo apt install gnome-characters gnome-calculator gnome-system-monitor \
 gnome-software-plugin-flatpak -yy
 
-# Purge Firefox, install Chromium:
-
-sudo apt purge firefox -yy
-sudo apt purge firefox-locale-en -yy
-if [ -d "/home/$USER/.mozilla" ]; then
-    rm -rf /home/$USER/.mozilla
-fi
-if [ -d "/home/$USER/.cache/mozilla" ]; then
-    rm -rf /home/$USER/.cache/mozilla
-fi
-
-sudo apt install chromium-browser
 
 gsettings set com.ubuntu.update-notifier show-livepatch-status-icon false
 #set icons to minimize on click
@@ -48,19 +31,9 @@ gsettings set org.gnome.shell.extensions.dash-to-dock click-action 'minimize'
 ## Remove junk
 sudo apt-get remove thunderbird rhythmbox ubuntu-web-launchers -y
 
-## Multimedia
-sudo apt-get install -y gimp scribus
-
-## Games
-sudo apt-get install -y steam-installer
-
-## Music
-curl -sS https://download.spotify.com/debian/pubkey.gpg | sudo apt-key add - 
-echo "deb http://repository.spotify.com stable non-free" | sudo tee /etc/apt/sources.list.d/spotify.list
-sudo apt-get update && sudo apt-get install spotify-client -y
 
 ## Text Editor
-sudo snap install --classic code
+#sudo snap install --classic code
 
 ## Disable Apport
 sudo sed -i 's/enabled=1/enabled=0/g' /etc/default/apport
